8a1325f5394b2055d144bd49920a6d96e3763b51
Houssam Righi <hrighi01@gmail.com>
2024-06-04 15:34:27 -0400

    Refacotred the home screen for api integration

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,103 +1,157 @@
 import 'package:flutter/material.dart';
+// Import your custom components
 import 'package:quick_inv/components/ComponentCard.dart';
 import 'package:quick_inv/components/CategoryCard.dart';
-
+// Assuming SearchBar is a custom widget, import it as well
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          color: const Color(0xFF0077b6),
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              const Text(
-                "Welcome to QuickInv! 🗄️",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    // Define the lists for categories and components
+    final List<Map<String, String>> categories = [
+      {"categoryName": "Capacitors"},
+      {"categoryName": "Resistors"},
+    ];
+
+    final List<Map<String, String>> components = [
+      {
+        "componentName": "Resistor OHM",
+        "categoryName": "Resistors",
+        "imgThumbnail": "assets/resist.jpg"
+      },
+    ];
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+            // App Bar
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF0077b6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
               ),
-              const SizedBox(height: 30),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment
-                    .start, // Align children horizontally at the start
+              child: Column(
                 children: [
+                  const SizedBox(height: 50),
+                  const Text(
+                    "QuickInv 🗄️",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment
+                        .start, // Align children horizontally at the start
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 24.0,
+                            bottom:
+                                10), // Add left padding to align with the title
+                        child: Text(
+                          "What are you looking for today?",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                  // Search Bar
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: SearchBar(hintText: "Search..."),
+                  ),
+
+                  // Search Buttons
+                  const SizedBox(height: 15),
                   Padding(
-                    padding: EdgeInsets.only(
-                        left: 20.0), // Add left padding to align with the title
-                    child: Text(
-                      "What are you looking for today?",
-                      style: TextStyle(fontSize: 18),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                            child: FilledButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Colors.black)),
+                          child: const Text("Search Tags"),
+                        )),
+                        const SizedBox(width: 10),
+                        Expanded(
+                            child: FilledButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Colors.black)),
+                          child: const Text("RFID Search"),
+                        ))
+                      ],
                     ),
                   ),
+                  const SizedBox(height: 20),
                 ],
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: SearchBar(hintText: "Search..."),
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FilledButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        backgroundColor:
-                        MaterialStatePropertyAll<Color>(Colors.black)),
-                    child: const Text("Search Tags"),
-                  ),
-                  const SizedBox(width: 10),
-                  FilledButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        backgroundColor:
-                        MaterialStatePropertyAll<Color>(Colors.black)),
-                    child: const Text("RFID Search"),
-                  )
-                ],
-              ),
-              const SizedBox(height: 20),
-            ],
-          ),
-        ),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
+            ),
+
+            // Home Screen Content/Body
             Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "Quick Categories",
-                style: TextStyle(fontSize: 18),
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    // Quick Categories
+                    const Text("Quick Categories",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: categories
+                          .map((category) => CategoryCard(
+                                categoryName: category["categoryName"]!,
+                              ))
+                          .toList(),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // View All Components
+                    const Text("View all components",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+
+                    Column(
+                      children: components
+                          .map((component) => ComponentCard(
+                                componentName: component["componentName"]!,
+                                categoryName: component["categoryName"]!,
+                                imgThumbnail: component["imgThumbnail"]!,
+                              ))
+                          .toList(),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
-        const Padding(
-          padding: EdgeInsets.all(10),
-          child: Row(children: [
-            CategoryCard(categoryName: "Capacitors"),
-            CategoryCard(categoryName: "Resistors")
-          ]),
-        ),
-        const Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: EdgeInsets.only(left: 8),
-            child: Text("View all components", style: TextStyle(fontSize: 20)),
-          ),
-        ),
-        const Column(
-          children: [
-            ComponentCard(
-                componentName: "Resistor OHM",
-                categoryName: "Resistors",
-                imgThumbnail: "assets/resist.jpg")
-          ],
-        )
-      ],
+      ),
     );
   }
 }
