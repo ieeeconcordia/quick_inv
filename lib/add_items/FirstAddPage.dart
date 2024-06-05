94640a50857bc6ce6c9c9809547ca5babe5af0ea
Houssam Righi <hrighi01@gmail.com>
2024-06-05 07:35:22 -0400

    UIpdated fetching

--- lib/add_items/FirstAddPage.dart
+++ lib/add_items/FirstAddPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:quick_inv/add_items/SecondAddPage.dart';
-import 'package:quick_inv/pbInstance.dart';
+import 'package:quick_inv/pocketbase.dart';
 
 class FirstAddPage extends StatefulWidget {
   const FirstAddPage({super.key});
@@ -16,13 +16,13 @@ class _FirstAddPageState extends State<FirstAddPage> {
 
   List<RecordModel> allTags = [];
 
-  List<String> entries = ["Resistor", "Capacitor", "Diode", "Other"];
-
   Future<void> fetchAllTags() async {
     try {
+      print("Fetching tags");
       List<RecordModel> tags =
           await pb.collection('tags').getFullList(sort: '-created');
       setState(() {
+        print(tags);
         allTags = tags;
       });
     } catch (e) {
@@ -39,51 +39,67 @@ class _FirstAddPageState extends State<FirstAddPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          "Add a new Item",
-          style: TextStyle(fontSize: 25),
+      appBar: AppBar(
+        title: const Text("Add New Item"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 64.0, 16.0, 0.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 15),
+            const Text(
+              "Add a new Item",
+              style: TextStyle(fontSize: 25),
+            ),
+            const SizedBox(height: 15),
+            const Text("Choose the type of component you want to add"),
+            const SizedBox(height: 15),
+
+            if (allTags.isNotEmpty)
+              DropdownMenu<String>(
+                initialSelection: "",
+                controller: typeController,
+                requestFocusOnTap: true,
+                label: const Text("Component Type"),
+                width: 250,
+                onSelected: (String? type) {
+                  setState(() {
+                    selectedType = type;
+                  });
+                },
+                dropdownMenuEntries: allTags
+                    .map<DropdownMenuEntry<String>>((RecordModel tag) {
+                  return DropdownMenuEntry<String>(
+                    value: tag.data['tag_name'],
+                    label: tag.data['tag_name'],
+                  );
+                }).toList(),
+              )
+            else
+              const CircularProgressIndicator(), // Show a loading indicator while fetching tags
+            const SizedBox(height: 20),
+            Center(
+              child: FilledButton(
+                onPressed: () async {
+                  await pb.collection('parts').create(body: {});
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SecondAddPage(),
+                    ),
+                  );
+                },
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.blue),
+                ),
+                child: const Text("Done"),
+              ),
+            ),
+          ],
         ),
-        const SizedBox(height: 15),
-        const Text("Choose the type of component you want to add"),
-        const SizedBox(height: 15),
-        Row(
-            children: allTags.map((tag) {
-          return Chip(label: Text(tag.data['name']));
-        }).toList()),
-        const SizedBox(height: 15),
-        DropdownMenu<String>(
-          initialSelection: "",
-          controller: typeController,
-          requestFocusOnTap: true,
-          label: const Text("Component Type"),
-          width: 250,
-          onSelected: (String? type) {
-            setState(() {
-              selectedType = type;
-            });
-          },
-          dropdownMenuEntries:
-              entries.map<DropdownMenuEntry<String>>((String entry) {
-            return DropdownMenuEntry<String>(
-              value: entry,
-              label: entry,
-            );
-          }).toList(),
-        ),
-        const SizedBox(height: 20),
-        FilledButton(
-          onPressed: () async {
-            await pb.collection('parts').create(body: {});
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SecondAddPage()));
-          },
-          style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blue)),
-          child: const Text("Done"),
-        )
-      ]),
-    ));
+      ),
+    );
   }
 }
